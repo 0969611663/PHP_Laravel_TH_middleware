11ae86d5e194ef2660e9d5d60bb784ee0835fe79
tu <nguyen.tu.st73@gmail.com>
2018-10-08 13:38:39 +0700

    ..

---
+++ database/migrations/2018_10_08_035949_create_customer_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_name', 50);
+            $table->string('account', 15);
+            $table->string('password', 15);
+            $table->date('birthday');
+            $table->string('address');
+            $table->string('email', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
---
+++ database/seeds/CustomerTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CustomerTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $dataArray = [];
+        for ($i = 0; $i < 20; $i++) {
+            array_push($dataArray, [
+                'user_name' => str_random(10),
+                'account' => str_random(10),
+                'password' => str_random(10),
+                'address' => str_random(10),
+                'birthday' => date("Y-m-d", mt_rand(1, time())),
+                'email' => str_random(10) . '@gmail.com',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ]);
+        }
+        DB::table('customer')->insert($dataArray);
+    }
+}
